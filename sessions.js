d27ef6eeda06b5af555b91338e523926926f77e7
oliver <gokohl@gmail.com>
2020-02-18 08:46:03 +0100

    new bot approach draft

---
+++ sessions.js
@@ -0,0 +1,100 @@
+const { request } = require('graphql-request');
+const aws = require("aws-sdk");
+const ddoc = require("dynamodb-doc");
+
+// query ID of all sessions in GraphCMS
+// query a session by given SIT hashtag
+const queryAllSessions = `query allSessions {
+  sessions {
+    id
+    title
+    recordingUrl
+    speakers {
+      firstName
+      lastName
+      twitterId
+    }
+    topics
+    event {
+      date
+      insideTrack {
+        hashtag
+        twitterId
+      }
+    }
+  }
+}`
+
+  
+const getAllSessions = async() => {
+  const data = await request(process.env.GRAPHCMS_ENDPOINT, queryAllSessions)
+  return data
+  // const rand = Math.floor(Math.random() * data.sessions.length)
+  // sessionId = data.sessions[rand].id
+  // console.log(sessionId)
+}
+
+// query a session by given speaker last name
+const querySessionBySpeaker = `query sessionBySpeaker($speaker: String) {
+  sessions(where: {speakers_every: {lastName_contains: $speaker}}) {
+    id
+    title
+    recordingUrl
+    speakers {
+      firstName
+      lastName
+      twitterId
+    }
+    topics
+    event {
+      date
+      insideTrack {
+        hashtag
+        twitterId
+      }
+    }
+  }
+}`
+
+const getBySit = function() {
+  return false
+}
+
+// query a session by given SIT hashtag
+const querySessionBySit = `query sessionBySit($sit: String) {
+  insideTracks(where: {hashtag: $sit}) {
+    events {
+      date
+      sessions {
+        id
+        title
+        speakers {
+          firstName
+          lastName
+          twitterId
+        }
+      }
+    }
+    hashtag
+    twitterId
+  }
+}`
+
+const getBySpeaker = async(speaker) => {
+  const data = await request(process.env.GRAPHCMS_ENDPOINT, querySessionBySpeaker, { speaker: speaker })
+  console.log(data)
+  return data
+}
+
+const update = function() {
+  // read full list of sessions from CMS
+  // update tweet state from local storage
+  // write sessions state to local storage
+}
+
+module.exports = {
+  update: update,
+  getBySit: getBySit,
+  getBySpeaker: getBySpeaker,
+  getAllSessions: getAllSessions
+}
