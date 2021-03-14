860f6b2d4c8ff678f18cf3ae93107d624c138408
oliver <gokohl@gmail.com>
2021-03-14 16:47:18 +0100

    authenticated requests against GraphCMS endpoint

--- opensit-twitter.js
+++ opensit-twitter.js
@@ -1,4 +1,4 @@
-const { request } = require('graphql-request');
+const { GraphQLClient, gql } = require('graphql-request');
 const commander = require('commander');
 const Twitter = require('twitter');
 
@@ -13,95 +13,96 @@ commander
   .option('-d, --debug', 'log mode')
   .parse(process.argv);
 
-// query ID of all sessions in GraphCMS
-const allSessions = `{
-  sessions {
-    id
-  }
-}`
-  
-// query details of session with given ID in GraphCMS
-const session = `query getSession($id: ID!) {
-  session(where: {id: $id}) {
-    id
-    title
-    speakers {
-      firstName
-      lastName
-      twitterId
+async function main() {
+  const graphQLClient = new GraphQLClient(process.env.GRAPHCMS_ENDPOINT, {
+    headers: {
+      authorization: 'Bearer '+process.env.GRAPHCMS_AUTHTOKEN,
+    },
+  })
+
+  // query ID of all sessions in GraphCMS
+  const queryAllSessions = `{
+    sessions {
+      id
     }
-    topics
-    event {
-      date
-      insideTrack {
-        hashtag
+  }`
+
+  // query details of session with given ID in GraphCMS
+  const querySession = `query getSession($id: ID!) {
+    session(where: {id: $id}) {
+      id
+      title
+      speakers {
+        firstName
+        lastName
         twitterId
       }
-    }
-  }
-}`
-
-request(
-  process.env.GRAPHCMS_ENDPOINT, allSessions
-).then(
-  data => {
-    let sessionId;
-    // either use gived sessions ID
-    if (commander.sessionid !== undefined) {
-      sessionId = commander.sessionid;
-    }
-    else {
-    // or use a random 
-      const randomSession = Math.floor(Math.random() * data.sessions.length);
-      sessionId = data.sessions[randomSession].id;
-    }
-
-    request(
-      process.env.GRAPHCMS_ENDPOINT, session, { id: sessionId }
-    ).then(
-      data => { 
-        const session = data.session;
-        if (commander.debug) console.log(session);
-        
-        const sessionDate = new Date(session.event.date);
-        const insideTrack = getInsideTrackIdText(
-          session.event.insideTrack.twitterId,
-          session.event.insideTrack.hashtag
-        );
-        const speakers = getSpeakerId(session.speakers);
-
-        const tweetText = getTweetText(
-          speakers,
-          session.title,
-          insideTrack,
-          session.event.insideTrack.hashtag,
-          sessionDate.getFullYear(),
-          session.topics
-        )
-        console.log('status text:\n'+tweetText);
-
-        if (commander.tweet) {
-          var twitterClient = new Twitter({
-            consumer_key: process.env.TWITTER_CONSUMER_KEY,
-            consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-            access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-            access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-          });
-          
-          twitterClient.post('statuses/update', { status: tweetText })
-          .then(function (tweet) {
-            console.log('tweet sent!\n');
-            console.log(tweet.text);
-            console.log(tweet.entities.hashtags);
-          })
-          .catch(function(error) {
-            throw error;
-          });
+      topics
+      event {
+        date
+        insideTrack {
+          hashtag
+          twitterId
         }
       }
-    )
+    }
+  }`
+
+  const allSessions = await graphQLClient.request(queryAllSessions);
+  if (commander.debug) console.log(JSON.stringify(allSessions));
+
+  let sessionId;
+  // either use gived sessions ID
+  if (commander.sessionid !== undefined) {
+    sessionId = commander.sessionid;
   }
-)
+  else {
+  // or use a random 
+    const randomSession = Math.floor(Math.random() * allSessions.sessions.length);
+    sessionId = allSessions.sessions[randomSession].id;
+  }
+
+  const data = await graphQLClient.request(querySession, { id: sessionId })
+
+  const session = data.session;
+  if (commander.debug) console.log(session);
+
+  const sessionDate = new Date(session.event.date);
+  const insideTrack = getInsideTrackIdText(
+    session.event.insideTrack.twitterId,
+    session.event.insideTrack.hashtag
+  );
+  const speakers = getSpeakerId(session.speakers);
+
+  const tweetText = getTweetText(
+    speakers,
+    session.title,
+    insideTrack,
+    session.event.insideTrack.hashtag,
+    sessionDate.getFullYear(),
+    session.topics
+  )
+  console.log('status text:\n'+tweetText);
+
+  if (commander.tweet) {
+    var twitterClient = new Twitter({
+      consumer_key: process.env.TWITTER_CONSUMER_KEY,
+      consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+      access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+      access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    });
+    
+    twitterClient.post('statuses/update', { status: tweetText })
+    .then(function (tweet) {
+      console.log('tweet sent!\n');
+      console.log(tweet.text);
+      console.log(tweet.entities.hashtags);
+    })
+    .catch(function(error) {
+      throw error;
+    });
+  }
+}
 
 function getInsideTrackIdText(twitterId, hashtag) {
   return (twitterId !== null && twitterId.length != 0) ? "@"+twitterId : "#"+hashtag;
@@ -151,3 +152,5 @@ function getSlug(title) {
 
   return title;
 }
+
+main();
